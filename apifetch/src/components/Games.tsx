bd4943eee13e4d79c4f2c8c9f2615c5f35aa305a
shaanar14 <shaanar14@users.noreply.github.com>
2021-10-20 14:56:41 +1100

    Search in the Games page working better

--- apifetch/src/components/Games.tsx
+++ apifetch/src/components/Games.tsx
@@ -8,32 +8,18 @@ const gamesEP = 'https://www.cheapshark.com/api/1.0/games?title=';
 
 const Games = (props?: {navInput:string}) => {
     const [games, setGames] = useState<gamesList>([]);
-    const [input, setInput] = useState<string>();
+    //const [input, setInput] = useState<string>();
+    const inputRef = useRef<HTMLInputElement>(null);
     const [btnPress, setBtnPress] = useState<boolean>(false);
-    let failCount = useRef<number>(0);
-    let error = useRef<boolean>(false);
-    const handleBtnPress = (evt: React.MouseEvent<HTMLButtonElement>): void => {
-        setBtnPress(!btnPress);
-    }
-    //TODO could make it so useEffect does not run on intial render using useRef, so that the user does not see UNDEFINED
-    //          and Touhou Seirensen Undefined Fantastic Object when they visit the page
     useEffect(() => {
         const fetchData = async () => {
-            failCount.current++;
-            //if the user has used the search bar in the nav bar use that value for the fetch request
-            let url = (typeof props !== "undefined") ? gamesEP + props.navInput : gamesEP + input;
-            const response = await fetch(gamesEP + input);
+            let url = gamesEP + inputRef.current!.value;
+            console.log(url);
+            const response = await fetch(url);
             //Error handling for fetch()
             if(!response.ok){
                 const message = "An error has occured" + response.status;
                 throw new Error(message)
-            }
-            if(failCount.current === 2)
-            {
-                error.current = true;
-                failCount.current = 0;
-                //const msg = "Forced API to fail";
-                //throw new Error(msg);
             }
             const json = await response.json().catch(error => {
                 console.log(error.message);
@@ -46,10 +32,10 @@ const Games = (props?: {navInput:string}) => {
         <div>
             <h1>Games</h1>
             {
-                error.current ? (error.current = false, <p>Oops your search could not be completed, try again later</p>) :
+               // error.current ? (error.current = false, <p>Oops your search could not be completed, try again later</p>) :
                 <div className="search-wrapper">
-                <input placeholder="Search..." type="text" value={input} onChange={e => setInput(e.target.value)}/>
-                <button onClick={e => handleBtnPress(e)}><MdSearch/></button>
+                <input placeholder="Search..." type="text" ref={inputRef}/>
+                <button onClick={() => setBtnPress(!btnPress)}><MdSearch/></button>
                 {
                     games.map((game) => (
                             <p key={game.gameID}>{game.external}</p>
