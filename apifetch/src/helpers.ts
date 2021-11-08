810ad2fb4d369d85e460762d23b2a1eec2306fa4
shaanar14 <shaanar14@users.noreply.github.com>
2021-11-08 13:22:21 +1100

    Restructed fetchData to be a helper function

--- apifetch/src/helpers.ts
+++ apifetch/src/helpers.ts
@@ -1,13 +1,9 @@
-import { gamesList } from './models/games.models'
-import { dealsList } from './models/deals.models'
-
-export const fetchData = async (url: string)=> {
+export const fetchData = async <T>(url: string): Promise<T | Error> => {
     const response = await fetch(url)
     if(!response.ok){
         console.log("Fetch error: ", response.status)
-        //TODO: ask if this is whats best to be returned from this function
         return new Error("Fetch failed: " + response.status)
     }
-    const json = await response.json()
+    const json: T = await response.json()
     return json
 }
--- apifetch/src/pages/Games.tsx
+++ apifetch/src/pages/Games.tsx
@@ -1,37 +1,36 @@
 import { useState, useEffect, useRef } from 'react'
 import { MdSearch } from 'react-icons/md';
 import { gamesList,endpoint, gameHeadings  } from '../models/games.models'
+import { fetchData } from '../helpers'
 import Table from '../components/table'
 import "../App.css"
-
 
 const Games = (props?: {navInput:string}) => {
     const inputRef = useRef<HTMLInputElement>(null);
     const [games, setGames] = useState<gamesList>([]);
     const [btnPress, setBtnPress] = useState<boolean>(false);
-    useEffect(() => {
-        const fetchData = async () => {
-            let url = endpoint + inputRef.current!.value;
-            const response = await fetch(url);
-            //Error handling for fetch()
-            if(!response.ok){
-                console.log("An error has occured" + response.status)
-                return new Error("An error has occured" + response.status)
+    const [error, setError] = useState<null | Error>()
+    useEffect (()  => {
+        let url = endpoint + inputRef.current!.value;
+        fetchData<gamesList>(url).then(x => {
+            if(Array.isArray(x)){
+                setGames(x)
+                setError(null)
             }
-            const json = await response.json()
-            setGames(json);
-        }
-        fetchData();
+            else setError(x)
+        });
+        console.log(games)
     }, [btnPress])
     return(
         <div>
             <h1>Games</h1>
             {
-               // error.current ? (error.current = false, <p>Oops your search could not be completed, try again later</p>) :
                 <div className="search-wrapper">
-                <input placeholder="Search..." type="text" ref={inputRef} onKeyPress={(e) =>{if(e.key === 'Enter') setBtnPress(!btnPress)}}/>
-                <button onClick={() => setBtnPress(!btnPress)}><MdSearch/></button>
-                <Table data={games} headings={gameHeadings}/>
+                    <input placeholder="Search..." type="text" ref={inputRef} onKeyPress={(e) =>{if(e.key === 'Enter') setBtnPress(!btnPress)}}/>
+                    <button onClick={() => setBtnPress(!btnPress)}><MdSearch/></button>
+                {
+                    error ? <p>Fetch Error</p> : <Table data={games} headings={gameHeadings}/>                  
+                }
                 </div>
             }
         </div>
